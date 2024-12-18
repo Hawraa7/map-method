a7ac84029f853d4f45980957dd28fd16420aeae1
Hawraa Hijazi <hawraahijazi1996@gmail.com>
2024-12-18 08:30:23 +0000

    Map method : num*2

--- map.js
+++ map.js
@@ -5,16 +5,27 @@
  */
 
 // Using a for loop
-
+let nums = [1, 2, 3, 4, 5];
+let results = [];
+for (let num of nums) {
+  results.push(num * 2)
+}
+console.log(results);
 
 // Using map()
-
+const multbytwo = function (num) {
+  return num * 2;
+}
+const mapresults = nums.map(multbytwo);
+console.log(mapresults);
 
 // Simplified w/ map()
-
+const simplified = nums.map(function (num) {return num * 2});
+console.log(simplified);
 
 // Simplfied w/ map() + arrow function
-
+const arrow = nums.map(num => num * 2);
+console.log(arrow);
 
 // With objects:
 const students = [
@@ -37,3 +48,5 @@ const students = [
     skill: 'CSS'
   },
 ];
+const studentswithIds = students.map(students => [students.name, students.id]);
+console.log(studentswithIds);
